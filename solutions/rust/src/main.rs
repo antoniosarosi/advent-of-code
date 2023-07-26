b679f8c621e45abcb3ec4d0e71f4eeb06fd32bf3
Antonio Sarosi <sarosiantonio@gmail.com>
2023-07-27 01:28:07 +0200

    Remove `clap` crate, parse CLI args manually

--- solutions/rust/src/main.rs
+++ solutions/rust/src/main.rs
@@ -1,38 +1,49 @@
-use clap::Parser;
+fn main() -> Result<(), String> {
+    let args: Vec<String> = std::env::args().collect();
 
-#[derive(Parser, Debug)]
-#[command(name = "aoc-rs")]
-/// Advent of Code Rust CLI
-struct Cli {
-    /// Advent of Code year
-    year: u32,
+    if args.len() < 3 || args.len() > 4 {
+        return Err(format!(
+            "Usage: {} YEAR DAY [path/to/alternate/input.txt]",
+            args[0]
+        ));
+    }
 
-    /// Advent of Code day
-    day: u8,
+    let year = args[1]
+        .parse::<u32>()
+        .map_err(|err| format!("Can't parse year '{}': '{err}'", args[1]))?;
 
-    /// Alternate input file
-    input: Option<std::path::PathBuf>,
-}
+    let day = args[2]
+        .parse::<u32>()
+        .map_err(|err| format!("Can't parse day '{}': '{err}'", args[2]))?;
 
-fn main() -> std::io::Result<()> {
-    let Cli { year, day, input } = Cli::parse();
+    let input_file = match args.get(3) {
+        Some(path) => std::path::PathBuf::from(path),
+
+        None => {
+            let executable = std::env::current_exe()
+                .map_err(|err| format!("Can't get executable path: {err}"))?;
+
+            let root_directory = executable
+                .ancestors()
+                .nth(5)
+                .ok_or("Executable file path is wrong. Use `cargo run`.")?;
+
+            root_directory.join(format!("inputs/{year}/day{day:02}.txt"))
+        }
+    };
+
     let solutions = rust::collect_solutions();
 
-    let key = format!("{year}/{day:02}");
+    let solution = solutions
+        .get(format!("{year}/{day:02}").as_str())
+        .ok_or(format!(
+            "Solution for year {year} day {day} is not implemented"
+        ))?;
 
-    let solution = solutions.get(key.as_str()).expect(&format!(
-        "Solution for year {year} day {day} is not implemented"
-    ));
+    let input = std::fs::read_to_string(&input_file)
+        .map_err(|err| format!("Cannot read input file '{}': {err}", input_file.display()))?;
 
-    let input = input.unwrap_or(
-        std::env::current_exe()?
-            .ancestors()
-            .nth(5)
-            .expect("input file path is wrong. Use `cargo run`.")
-            .join(format!("inputs/{year}/day{day:02}.txt")),
-    );
-
-    let (part1, part2) = solution(&std::fs::read_to_string(input)?);
+    let (part1, part2) = solution(&input);
 
     println!("{part1}\n{part2}");
 
