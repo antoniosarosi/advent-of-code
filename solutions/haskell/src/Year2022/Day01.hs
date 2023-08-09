ea0f0630790b51e1639e5f20742d51dffbc1a05c
Antonio Sarosi <sarosiantonio@gmail.com>
2023-08-09 17:06:12 +0200

    Alternative solutions to part1 and part2

--- solutions/haskell/src/Year2022/Day01.hs
+++ solutions/haskell/src/Year2022/Day01.hs
@@ -4,13 +4,16 @@ import Data.List (sortOn)
 import Data.List.Split (splitOn)
 
 parse :: String -> [Int]
-parse input = map sum $ map (map read) $ map words $ splitOn "\n\n" input
+-- parse input = map sum $ map (map read) $ map words $ splitOn "\n\n" input
+parse = map sum . map (map read) . map words . splitOn "\n\n"
 
 part1 :: [Int] -> Int
-part1 calories = maximum calories
+-- part1 calories = maximum calories
+part1 = maximum
 
 part2 :: [Int] -> Int
-part2 calories = sum $ take 3 $ sortOn negate calories
+-- part2 calories = sum $ take 3 $ sortOn negate calories
+part2 = sum . take 3 . sortOn negate
 
 solution :: String -> (String, String)
 solution input = (show $ part1 calories, show $ part2 calories)
