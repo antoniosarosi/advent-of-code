02498cfee7684369857f3f333eeee36af9feb0c8
Antonio Sarosi <sarosiantonio@gmail.com>
2023-08-09 16:43:07 +0200

    2022 Day 1 haskell

--- solutions/haskell/src/Year2022/Day01.hs
+++ solutions/haskell/src/Year2022/Day01.hs
@@ -1,4 +1,18 @@
 module Year2022.Day01 (solution) where
 
+import Data.List (sortOn)
+import Data.List.Split (splitOn)
+
+parse :: String -> [Int]
+parse input = map sum $ map (map read) $ map words $ splitOn "\n\n" input
+
+part1 :: [Int] -> Int
+part1 calories = maximum calories
+
+part2 :: [Int] -> Int
+part2 calories = sum $ take 3 $ sortOn negate calories
+
 solution :: String -> (String, String)
-solution input = ("Todo: Part 1 called with input " ++ input, "Todo: Part 2 " ++ input)
+solution input = (show $ part1 calories, show $ part2 calories)
+  where
+    calories = parse input
