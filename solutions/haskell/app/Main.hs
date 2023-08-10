cd7e148022f340de70a1b5963efc7c092ff36af3
Antonio Sarosi <sarosiantonio@gmail.com>
2023-08-10 21:38:43 +0200

    Fix Haskell `main`

--- solutions/haskell/app/Main.hs
+++ solutions/haskell/app/Main.hs
@@ -1,7 +1,7 @@
 module Main (main) where
 
 import qualified Data.Map as Map
-import Data.Maybe (fromJust, isNothing)
+import Data.Maybe (fromMaybe)
 import System.Environment (getArgs)
 import System.Exit (exitFailure)
 import System.IO (hPutStrLn, stderr)
@@ -10,35 +10,36 @@ import Text.Read (readMaybe)
 
 import Solutions (solutions)
 
-die :: String -> IO ()
+data Args = Args Int Int (Maybe String)
+
+parseArgs :: [String] -> Either String Args
+parseArgs args = case args of
+    [year, day] -> ($ Nothing) <$> parseYearAndDay year day
+    [year, day, path] -> ($ Just path) <$> parseYearAndDay year day
+    _ -> Left "Usage: ./executable YEAR DAY [optional/input.txt]"
+  where
+    parseYearAndDay year day = case (readMaybe year, readMaybe day) of
+        (Nothing, _) -> Left ("Can't parse year: " ++ year)
+        (_, Nothing) -> Left ("Can't parse day: " ++ day)
+        (Just y, Just d) -> Right $ Args y d
+
+die :: String -> IO a
 die msg = hPutStrLn stderr msg >> exitFailure
+
+readCliArgs :: [String] -> IO Args
+readCliArgs args = case parseArgs args of
+    Left err -> die err
+    Right parsed -> return parsed
 
 main :: IO ()
 main = do
-    args <- getArgs
-    if length args < 2 || length args > 3
-        then die "Usage: ./executable YEAR DAY [optional/input.txt]"
-        else do
-            let maybeYear = readMaybe (head args) :: Maybe Int
-            if isNothing maybeYear
-                then die ("Cant parse year: " ++ head args)
-                else do
-                    let maybeDay = readMaybe (args !! 1) :: Maybe Int
-                    if isNothing maybeDay
-                        then die ("Cant parse day: " ++ (args !! 1))
-                        else do
-                            let year = fromJust maybeYear
-                            let day = fromJust maybeDay
-                            let key = show year ++ "/" ++ printf "%02d" day
+    Args year day maybeInputFile <- readCliArgs =<< getArgs
+    let key = printf "%d/%02d" year day
+    let inputFile = fromMaybe (printf "../../inputs/%d/day%02d.txt" year day) maybeInputFile
 
-                            let inputFile =
-                                    if length args == 3
-                                        then args !! 2
-                                        else printf "../../inputs/%d/day%02d.txt" year day
-
-                            case Map.lookup key solutions of
-                                Nothing -> die ("Solution for year " ++ head args ++ " and day " ++ (args !! 1) ++ " not found")
-                                Just solution -> do
-                                    input <- readFile inputFile
-                                    let (part1, part2) = solution input
-                                    putStrLn (part1 ++ "\n" ++ part2)
+    case Map.lookup key solutions of
+        Nothing -> die $ printf "Solution for year %d and day %d not found" year day
+        Just solution -> do
+            input <- readFile inputFile
+            let (part1, part2) = solution input
+            putStrLn (part1 ++ "\n" ++ part2)
